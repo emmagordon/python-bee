4ffadf3d4f30b456b6235200d7479c221102ed84
Emma Gordon <emmagordon@users.noreply.github.com>
2017-07-14 13:21:19 +0200

    Clarify bases question

--- hard/bases.py
+++ hard/bases.py
@@ -3,7 +3,9 @@
 returns a string containing the digits of the first number in the base
 of the second number.
 
-You may assume the base is between 2 and 10 (inclusive).
+If the base is larger than 10, you should use the alphabet for digits
+beyond 9, e.g. 'a' for 10. You may assume you won't need anything
+larger than 'z'!
 
 >>> f(123758, 10)
 '123758'
@@ -15,6 +17,10 @@ You may assume the base is between 2 and 10 (inclusive).
 '1021'
 >>> f(15, 8)
 '17'
+>>> f(28, 16) in ['1c', '1C']
+True
+>>> f(19, 20) in ['j', 'J']
+True
 """
 
 import doctest
